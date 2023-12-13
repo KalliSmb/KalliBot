e662b49d223d193efca06a14cc43ba5f68b413c4
Kayro <kayromatheus584@gmail.com>
2023-12-13 18:24:03 +0000

    Comando test atualizado para modo dev only

--- src/commands/misc/test.js
+++ src/commands/misc/test.js
@@ -7,6 +7,6 @@ module.exports = {
     // options: Object[],
 
     callback: (client, interaction) => {
-        interaction.reply("<:OMEGALUL:715527251889881099>");
+        interaction.reply("<:OMEGALUL:715527251889881099>"); // nome emote + id emote
     },
 };
