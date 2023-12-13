2ff58e736dffb7367ecad241cb5b3f40ee421700
Kayro <kayromatheus584@gmail.com>
2023-12-13 18:30:59 +0000

    Mensagem de erro na consola modificada

--- src/events/interactionCreate/handleCommands.js
+++ src/events/interactionCreate/handleCommands.js
@@ -60,6 +60,6 @@ module.exports = async (client, interaction) => {
         await commandObject.callback(client, interaction);
 
     } catch (error) {
-        console.log(`There was an error running this command: ${error}`);
+        console.log(`Erro ao executar o comando: ${error}`);
     }
 };
